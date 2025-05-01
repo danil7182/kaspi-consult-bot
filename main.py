f0b3ecea9a1fb0b5032b79d89d98338306b914c4
danil7182 <danik8181@gmail.com>
2025-05-01 14:18:55 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-python-telegram-bot==20.4
-openai
-python-dotenv
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+# ТВОЙ ТОКЕН ОТ BOTFATHER
+BOT_TOKEN = "8128516914:AAFWCXThDbhTK0OsVaITbc2emCHBTVe1oOs"
+
+# Обработчик команды /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Привет! Оплати 1000₸ на Kaspi и пришли скриншот оплаты сюда. "
+        "После подтверждения ты получишь доступ к чату на 20 минут."
+    )
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    # Регистрируем обработчик команды /start
+    app.add_handler(CommandHandler("start", start))
+
+    print("Бот запущен...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
